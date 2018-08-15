44c949e361189451966680e29b844704451e5783
magu <s.yamaguti513@gmail.com>
2018-08-16 01:06:17 +0900

    Add template syntax chaptor #2

---
+++ vue-template/index.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+    el: "#app",
+    data: {
+        msg: "Hello World"
+    }
+});
+
+const app2 = new Vue({
+    el: "#app-2",
+    data: {
+        msg: "Once only"
+    }
+});
