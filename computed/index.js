c6272e5a4c92b8965541f8ab2f8b565374f508d3
magu <s.yamaguti513@gmail.com>
2018-08-18 16:47:01 +0900

    ADD watch properties and computed setter

--- computed/index.js
+++ computed/index.js
@@ -14,3 +14,50 @@ const vm = new Vue({
         }
     }
 });
+
+const vm2 = new Vue({
+    el: "#demo",
+    data: {
+        firstName: 'Foo',
+        lastName: 'Bar',
+        fullName: 'Foo Bar'
+    },
+    // firstName,lastNameの変更を監視する。
+    // この場合だと冗長なので、computedを利用した方が良い(大抵はcomputedの方がいいらしい)
+    watch: {
+        firstName: function (val) {
+            this.fullName = val + ' ' + this.lastName
+        },
+        lastName: function (val) {
+            this.fullName =  this.firstName + ' ' + val
+        }
+    }
+    /*
+    // computedを利用した例。
+    computed: {
+        fullName: function () {
+            return this.firstName + ' ' + this.lastName
+        }
+    }
+    */
+});
+
+const vm3 = new Vue({
+    el: "#setter",
+    data: {
+        firstName: 'Foo',
+       lastName: 'Bar'
+    },
+    computed: {
+       fullName: {
+           get: function () {
+               return this.firstName + ' ' + this.lastName
+           },
+           set: function (newValue) {
+               let names = newValue.split(' ')
+               this.firstName = names[0]
+               this.lastName = names[1]
+           }
+       }
+    }
+});
